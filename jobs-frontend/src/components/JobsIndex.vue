de12fc6c2d47edd59a90eab0d753072f6b79703e
Jackson <jwreynold@gmail.com>
2021-02-03 16:26:47 -0500

    copied all files into frontend directory

---
+++ jobs-frontend/src/components/JobsIndex.vue
@@ -0,0 +1,42 @@
+<template>
+    <div id='jobs-index'>
+        <ul>
+            <li v-for="job in jobs" :key="job.name">{{job.name}}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import EditJob from './EditJob'
+    import ShowJob from './ShowJob'
+
+    export default {
+        name: 'JobsIndex',
+        components: {EditJob, ShowJob},
+        data () {
+            return {
+                jobs: []
+            }
+        },
+        mounted () {
+            localStorage.jobs ? null : this.fetchJobs()
+        },
+        methods: {
+            fetchJobs() {
+                fetch('https://localhost:3000/jobs')
+                  .then(r => r.json())
+                  .then(r => localStorage.jobs = r.jobs)
+                  .catch(() => {
+                      alert('Please check your server, reload, and try again')
+                  })
+            }
+        },
+        computed: {
+
+        }
+    }
+</script>
+
+<style lang="sass" scoped>
+
+</style>
