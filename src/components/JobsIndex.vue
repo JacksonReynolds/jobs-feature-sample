97fd5ba5bc0ef47ea2851b3e63a3a3c5af6a2bbd
Jackson <jwreynold@gmail.com>
2021-02-03 14:48:49 -0500

    building index

--- src/components/JobsIndex.vue
+++ src/components/JobsIndex.vue
@@ -5,8 +5,22 @@
 </template>
 
 <script>
+    import EditJob from './EditJob'
+    import ShowJob from './ShowJob'
+
     export default {
-        
+        components: {EditJob, ShowJob},
+        data () {
+            return {
+                jobs: []
+            }
+        },
+        methods: {
+
+        },
+        computed: {
+
+        }
     }
 </script>
 
