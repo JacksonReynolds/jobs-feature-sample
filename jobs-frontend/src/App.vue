3537a0b0cf69d58bd15f86eaea55d296192b7e42
Jackson <jwreynold@gmail.com>
2021-02-04 12:47:46 -0500

    switch to jobs index

--- jobs-frontend/src/App.vue
+++ jobs-frontend/src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <JobsIndex/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import JobsIndex from './components/JobsIndex.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    JobsIndex
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
