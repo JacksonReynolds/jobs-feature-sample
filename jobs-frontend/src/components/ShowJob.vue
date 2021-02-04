2c8dc1437a7e42f9a523724df3cef7337010882b
Jackson <jwreynold@gmail.com>
2021-02-04 14:58:33 -0500

    basic attr presentation w/o buttons

--- jobs-frontend/src/components/ShowJob.vue
+++ jobs-frontend/src/components/ShowJob.vue
@@ -1,15 +1,33 @@
 <template>
-    <div>
-
+    <div class="show-job" :id="id">
+      <p>Title: {{title}}</p>
+      <p>Skills:</p>
+      <ul>
+        <li v-for="skill in skills" :key="skill">{{skill}}</li>  
+      </ul>
+      <p>Description: {{description}}</p>
     </div>
 </template>
 
 <script>
     export default {
-        
+        name: "JobShow",
+        props: {
+          job: Object
+        },
+        data () {
+          return {
+            id: this.job.id,
+            title: this.job.title,
+            skills: this.job.skills,
+            description: this.job.description
+          }
+        },
+        methods: {
+        }
     }
 </script>
 
-<style lang="sass" scoped>
+<style scoped>
 
 </style>
