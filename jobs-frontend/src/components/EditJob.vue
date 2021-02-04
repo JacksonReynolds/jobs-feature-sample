78f6292d34e805ddb5818e3fcbdec7b99f953c8a
Jackson <jwreynold@gmail.com>
2021-02-04 17:20:47 -0500

    binding data and stubbing skill deletion handling

--- jobs-frontend/src/components/EditJob.vue
+++ jobs-frontend/src/components/EditJob.vue
@@ -1,15 +1,38 @@
 <template>
-    <div>
-
-    </div>
+  <div class="edit-job" :id="id">
+    <p>Title: <input type="text" v-model="title"></p>
+      <p>Skills:</p>
+      <ul>
+        <li v-for="(skill, index) in skills" :key="skill"><input type="text" v-model="skills[index]"><button @click="() => deleteSkill(skill)"></button></li>
+        <li id="new"><input type="text" v-model="newSkill"></li>  
+      </ul>
+      <p>Description: <input type="text" v-model="description"></p>
+  </div>
 </template>
 
 <script>
-    export default {
-        
+  export default {
+    name: "EditJob",
+    props: {
+      job: Object
+    },
+    data () {
+      return {
+        id: this.job.id,
+        title: this.job.title,
+        skills: this.job.skills,
+        newSkill: '',
+        description: this.job.description,
+      }
+    },
+    methods: {
+      deleteSkill(skill) {
+        console.log(`${skill} deleted...?`)
+      }
     }
+  }
 </script>
 
-<style lang="sass" scoped>
+<style scoped>
 
 </style>
