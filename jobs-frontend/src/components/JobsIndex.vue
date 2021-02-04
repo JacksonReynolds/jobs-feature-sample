1a97acff9a367a019ab1f8f6512d7f8933b2ceaf
Jackson <jwreynold@gmail.com>
2021-02-04 14:59:10 -0500

    implement ShowJob component

--- jobs-frontend/src/components/JobsIndex.vue
+++ jobs-frontend/src/components/JobsIndex.vue
@@ -5,38 +5,35 @@
       <input>
     </header>
     <ul>
-      <li v-for="job in jobs" :key="job.name">
-        Title: {{job.title}}<br>
-        <ul>Skills:
-          <li v-for="skill in job.skills" :key="skill">{{skill}}</li>
-        </ul><br>
-        Description: {{job.description}}
-      </li>
+      <ShowJob v-for="job in jobs" :job="job" :key="job.id"/>
     </ul>
   </div>
 </template>
 
 <script>
   // import EditJob from './EditJob'
-  // import ShowJob from './ShowJob'
+  import ShowJob from './ShowJob'
   export default {
     name: 'JobsIndex',
-    // components: {EditJob, ShowJob},
+    components: {ShowJob},
     data () {
       return {
         jobs: [{
+          id: 1,
           title: "Full-Stack Engineer",
           skills: ["JavaScript", "Ruby", "Rails", "Vue.js", "HTML", "CSS"],
           description: `As a Full Stack Engineer, you will aid us in innovating the world
                         of connecting people with a job that they love.`
         },
         {
+          id: 2,
           title: "Architect",
           skills: ["CAD", "Revit", "Residential and Commericla Design", "Building Codes"],
           description: `This position is excellent for architects seeking career growth within the 
                         firm, design opportunities in a variety of project types, and extensive client contact.`
         },
         {
+          id: 3,
           title: "Real Estate Agent",
           skills: ["Market Analysis", "Salespersonship", "Networking", "Presentation"],
           description: `A Real Estate Agent plays an essential role in a real estate transaction.
@@ -45,6 +42,7 @@
                         with every detail of their home purchase and sale.`
         },
         {
+          id: 4,
           title: "Fitness Trainer",
           skills: [""],
           description: `Instruct members and guests using up to date techniques and methods to ensure
@@ -74,8 +72,8 @@
 #jobs-index-header {
   display: flex;
   justify-content: space-between;
-  font-family: Arvo;
-  
+  font-family: Arial;
+  padding: 10px;
 }
 #jobs-index {
   display: flex;
