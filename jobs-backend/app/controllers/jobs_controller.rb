ac40225e1df4193e74a6142278a9f16873d42a6a
Jackson <jwreynold@gmail.com>
2021-02-05 18:05:25 -0500

    create, update, strong params

--- jobs-backend/app/controllers/jobs_controller.rb
+++ jobs-backend/app/controllers/jobs_controller.rb
@@ -2,4 +2,28 @@ class JobsController < ApplicationController
   def index
     render json: Job.all
   end
+
+  def create
+    job = Job.new(job_params)
+    if job.save
+      render json: job, :status => 201
+    else
+      render json: {msg: job.errors.full_messages.join}, :status => 500
+    end
+  end
+
+  def update
+    job = Job.find_by(id: params[:id])
+    if job.update(job_params)
+      render json: job, :status => 200
+    else
+      render json: 
+    end
+  end
+
+  private
+
+  def job_params
+    params.require(:job).permit(:title, :description, :skills)
+  end
 end
