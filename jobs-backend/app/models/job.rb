5951f1757726ce56dd88dbaefdb9b2e48db85090
Jackson <jwreynold@gmail.com>
2021-02-05 19:04:51 -0500

    job model validations

--- jobs-backend/app/models/job.rb
+++ jobs-backend/app/models/job.rb
@@ -1,2 +1,10 @@
 class Job < ApplicationRecord
+  validates :title, :description, presence: true
+  validate :has_at_least_one_skill
+
+  def has_at_least_one_skill
+    if skills.split(' ').length < 1
+      errors.add(:skills, "There must be at least one skill")
+    end
+  end
 end
