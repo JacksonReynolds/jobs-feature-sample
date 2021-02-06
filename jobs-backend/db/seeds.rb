053d1c6855667f4d13aa5972a40ab70678ee1119
Jackson <jwreynold@gmail.com>
2021-02-05 20:46:49 -0500

    switching back to arrays

--- jobs-backend/db/seeds.rb
+++ jobs-backend/db/seeds.rb
@@ -9,22 +9,22 @@
 Job.create([
   {
     title: "Full-Stack Engineer",
-    skills: ["JavaScript", "Ruby", "Rails", "Vue.js", "HTML", "CSS"].join(' '),
+    skills: ["JavaScript", "Ruby", "Rails", "Vue.js", "HTML", "CSS"],
     description: "As a Full Stack Engineer, you will aid us in innovating the world of connecting people with a job that they love."
   },
   {
     title: "Architect",
-    skills: ["CAD", "Revit", "Residential and Commercial Design", "Building Codes"].join(' '),
+    skills: ["CAD", "Revit", "Residential and Commercial Design", "Building Codes"],
     description: "This position is excellent for architects seeking career growth within the firm, design opportunities in a variety of project types, and extensive client contact."
   },
   {
     title: "Real Estate Agent",
-    skills: ["Market Analysis", "Salespersonship", "Networking", "Presentation"].join(' '),
+    skills: ["Market Analysis", "Salespersonship", "Networking", "Presentation"],
     description: "A Real Estate Agent plays an essential role in a real estate transaction. In this role, you will advocate for your clients by understanding their preferences, representing them when negotiating a sale and assisting them with every detail of their home purchase and sale."
   },
   {
     title: "Fitness Trainer",
-    skills: ["Knowledge of Anatomy", "Record Keeping", "Principles of Nutrition", "Affective Exercises", "Personal Training Techniques"].join(' '),
+    skills: ["Knowledge of Anatomy", "Record Keeping", "Principles of Nutrition", "Affective Exercises", "Personal Training Techniques"],
     description: "Instruct members and guests using up to date techniques and methods to ensure proper use of all exercise equipment as well as other methods of exercise."
   }
 ])
