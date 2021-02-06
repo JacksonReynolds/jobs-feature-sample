2a3427961ae089f9eb08a7576d2784f1703f42c9
Jackson <jwreynold@gmail.com>
2021-02-05 20:23:41 -0500

    moved to rails db

--- jobs-frontend/src/components/JobsIndex.vue
+++ jobs-frontend/src/components/JobsIndex.vue
@@ -24,46 +24,23 @@
     data () {
       return {
         filterTerm: '',
-        jobs: [{
-          id: 1,
-          title: "Full-Stack Engineer",
-          skills: ["JavaScript", "Ruby", "Rails", "Vue.js", "HTML", "CSS"],
-          description: "As a Full Stack Engineer, you will aid us in innovating the world of connecting people with a job that they love.",
-          editing: false
-        },
-        {
-          id: 2,
-          title: "Architect",
-          skills: ["CAD", "Revit", "Residential and Commercial Design", "Building Codes"],
-          description: "This position is excellent for architects seeking career growth within the firm, design opportunities in a variety of project types, and extensive client contact.",
-          editing: false
-        },
-        {
-          id: 3,
-          title: "Real Estate Agent",
-          skills: ["Market Analysis", "Salespersonship", "Networking", "Presentation"],
-          description: "A Real Estate Agent plays an essential role in a real estate transaction. In this role, you will advocate for your clients by understanding their preferences, representing them when negotiating a sale and assisting them with every detail of their home purchase and sale.",
-          editing: false
-        },
-        {
-          id: 4,
-          title: "Fitness Trainer",
-          skills: ["Knowledge of Anatomy", "Record Keeping", "Principles of Nutrition", "Affective Exercises", "Personal Training Techniques"],
-          description: "Instruct members and guests using up to date techniques and methods to ensure proper use of all exercise equipment as well as other methods of exercise.",
-          editing: false
-        }]
+        jobs: []
       }
     },
-    // mounted () {
-    //   localStorage.jobs ? this.jobs = localStorage.jobs : this.fetchJobs()
-    // },
+    mounted () {
+      localStorage.jobs ? this.jobs = localStorage.jobs : this.fetchJobs()
+    },
     methods: {
       fetchJobs() {
-        fetch('https://localhost:3000/jobs')
+        fetch('http://localhost:3000/jobs')
           .then(r => r.json())
-          .then(r => localStorage.jobs = r.jobs)
-          .catch(() => {
-            alert('Please check your server, reload, and try again')
+          .then(jobs => {
+            console.log(jobs)
+            // localStorage.jobs = jobs
+            // this.jobs = jobs
+            })
+          .catch(({msg}) => {
+            alert(`Please check your server, reload, and try again, ${msg}`)
           })
       },
       fetchUpdatedJob(job) {
